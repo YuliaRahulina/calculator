d1d0b35e54454211fdc9952d0e0e91d1bcf78ae1
Yulia <yulia.rahulina@gmail.com>
2023-07-10 15:46:21 +0300

    init project

---
+++ src/calculator.js
@@ -0,0 +1,84 @@
+function seven() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 7
+}
+
+function eight() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 8
+}
+
+function nine() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 9
+}
+
+function division() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + "/"
+}
+
+function four() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 4
+}
+
+function five() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 5
+}
+
+function six() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 6
+}
+
+function asterisk() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + "*"
+}
+
+function one() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 1
+}
+
+function two() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 2
+}
+
+function three() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 3
+}
+
+function minus() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + "-"
+}
+
+function zero() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + 0
+}
+
+function clean() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value.slice(0, resultInput.value.length - 1)
+}
+
+function equals() {
+    let resultInput = document.getElementById("result")
+    resultInput.value =  eval(resultInput.value)
+}
+
+function plus() {
+    let resultInput = document.getElementById("result")
+    resultInput.value = resultInput.value + "+"
+}
+
+
+
+
+
